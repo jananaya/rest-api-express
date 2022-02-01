6a0e01c720394572763b06a4fb9170b57e5d78c0
jananaya <jancarlosanayajimenez@gmail.com>
2022-01-31 23:51:47 -0500

    database products

---
+++ src/database.ts
@@ -0,0 +1,70 @@
+import * as mysql from 'mysql';
+import { Connection } from 'mysql';
+import { Product } from './interfaces/Product';
+import 'dotenv/config';
+
+export class Database {
+    private connection: Connection;
+    private tableName: string;
+
+    constructor() {
+        this.tableName = 'Products';
+        this.connection = mysql.createConnection({
+            host: process.env.HOST,
+            user: process.env.user,
+            password: process.env.password,
+            database: process.env.database
+        });
+
+        this.connection.connect((err) => {
+            if (err) throw err;
+        });
+    }
+
+    addRegister(data: Product) {
+        const insertQuery = `INSERT INTO ${this.tableName} (\`description\`, \`value\`)
+                             VALUES (?, ?)`;
+        const query = mysql.format(insertQuery, [data.description, data.value]);
+
+        return new Promise((resolve, reject) => {
+            this.connection.query(query, (err) => {
+                if (err) reject(err);
+                else resolve('Product created successfully!');
+            })
+        });
+    }
+
+    getRegister(id: number): Promise<Product> {
+        const query = `SELECT * FROM ${this.tableName} WHERE id=${id}`;
+
+        return new Promise<Product>((resolve, reject) => {
+            this.connection.query(query, (err, result) => {
+                if (err) reject(err);
+                else resolve(result);
+            })
+        });
+    }
+
+    deleteRegister(id: number) {
+        const query = `DELETE FROM ${this.tableName} WHERE id=${id}`;
+
+        return new Promise((resolve, reject) => {
+            this.connection.query(query, (err) => {
+                if (err) reject(err);
+                else resolve('Product deleted successfully!');
+            })
+        });
+    }
+
+    updateRegister(id: number, data: Product) {
+        const insertQuery = `UPDATE ${this.tableName} SET description=?, value=? WHERE id=${id}`;
+        const query = mysql.format(insertQuery, [data.description, data.value]);
+
+        return new Promise((resolve, reject) => {
+            this.connection.query(query, (err) => {
+                if (err) reject(err);
+                else resolve('Product created successfully!');
+            })
+        })
+    }
+}
