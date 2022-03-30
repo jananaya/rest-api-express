c9e9aa086cd506cf7c2b7193b21723463eaea6b0
jananaya <jancarlosanayajimenez@gmail.com>
2022-03-30 15:43:58 -0500

    modify product controllers for use repository

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -1,28 +1,32 @@
-import {Database} from '../database';
-import {NextFunction, Request, Response} from 'express';
-import {responseSuccessBody} from '../utils/responseSuccessBody';
+import { Database } from '../schema/Database';
+import { NextFunction, Request, Response } from 'express';
+import { responseSuccessBody } from '../utils/responseSuccessBody';
+import { ProductMapper } from '../schema/ProductMapper';
+import { ProductRepo } from '../schema/ProductRepo';
 
-const database = new Database();
+const database = Database.getInstance();
+const productRepo = new ProductRepo(database);
 
 export namespace productsControllers {
     export const createProduct = (req: Request, res: Response, next: NextFunction) => {
-        database.addProduct(req.body)
+        productRepo.create(req.body)
             .then(message => res.json(responseSuccessBody(message)))
             .catch(error => next(error))
     }
 
     export const readProduct = (req: Request, res: Response, next: NextFunction) => {
         const id = parseInt(req.params.id);
+        const productPromise = productRepo.read(id);
 
-        database.getProduct(id)
-            .then(product => res.json(product))
+        productPromise
+            .then(product => res.json(ProductMapper.toDTO(product)))
             .catch(error => next(error))
     }
 
     export const updateProduct = (req: Request, res: Response, next: NextFunction) => {
         const id = parseInt(req.params.id);
 
-        database.updateProduct(id, req.body)
+        productRepo.update(id, req.body)
             .then((message) => res.json(responseSuccessBody(message)))
             .catch(error => next(error))
     }
@@ -30,7 +34,7 @@ export namespace productsControllers {
     export const deleteProduct = (req: Request, res: Response, next: NextFunction) => {
         const id = parseInt(req.params.id);
 
-        database.deleteProduct(id)
+        productRepo.delete(id)
             .then((message) => res.json(responseSuccessBody(message)))
             .catch(error => next(error));
     }
