f66440e37d7289fd7051123f80300c3f87879482
jananaya <jancarlosanayajimenez@gmail.com>
2022-04-28 19:10:39 -0500

    install and use CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import { products } from './routes/products.routes';
 import { error } from './middleware/error.middlewares';
@@ -6,6 +7,7 @@ import { error } from './middleware/error.middlewares';
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/products', products);
